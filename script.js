1c93b62202f1d11add7737108103d010c8b41ff0
Laura Gynther <lauragynther@live.com>
2022-05-25 13:18:58 +0300

    lisättiin vuoro

--- script.js
+++ script.js
@@ -6,25 +6,17 @@ let closebtn = document.getElementById("closeModal");
 let play = document.getElementById("roll");
 
 //Kuvat:
-
 let img1 = document.getElementById("dice1");
 let img2 = document.getElementById("dice2");
 let img3 = document.getElementById("dice3");
 
+//Yhdellä vai kahdella nopalla:
 let area1 = document.getElementById("dice");
 let area2 = document.getElementById("dices");
 
 const images = ['dice1.png', 'dice2.png', 'dice3.png', 'dice4.png', 'dice5.png', 'dice6.png'];
 
-//Pelaajien nimet:
-let name1 = document.getElementById("name1").value;
-let name2 = document.getElementById("name2").value;
-let name3 = document.getElementById("name3").value;
-let name4 = document.getElementById("name4").value;
-let name5 = document.getElementById("name5").value;
-
-let pointArray = [0,1,2,3,4];
-
+//Objekti array:
 let players = [
     {
         name: name1,
@@ -51,7 +43,7 @@ let players = [
 let pisteet = [];
 let points = 0;
 let tulostus = "";
-
+let vuoro = 0;
 let ekaluku = 0;
 let tokaluku = 0;
 
@@ -68,6 +60,12 @@ closebtn.onclick = function(){
 
 //Lataa valitun/valitut nopat sekä tulostaa pelaajat+pisteet
 function upDate(){
+
+    players[0].name = document.getElementById("name1").value;
+    players[1].name = document.getElementById("name2").value;
+    players[2].name = document.getElementById("name3").value;
+    players[3].name = document.getElementById("name4").value;
+    players[4].name = document.getElementById("name5").value;
     
     if (document.getElementById("oneDice").checked){
         area1.style.display="block";
@@ -103,11 +101,9 @@ function roll(){
         pisteet.push(tokaluku);
        /* if (ekaluku == 0 || tokaluku == 0){
             document.getElementById("alertArea").innerHTML = "You rolled 1 ! Zero points!";
-            delete pisteet[pisteet.length];
-        }*/
-        console.log(pisteet);
-        //points = points + ekaluku + 1 + tokaluku +1;
-    } 
+            delete pisteet[pisteet.length];*/
+    }
+        
     if (document.getElementById("oneDice").checked){
         ekaluku = Math.floor((Math.random() * [images.length]));
         img3.src = `diceimg/${[images[ekaluku]]}`;
@@ -115,23 +111,26 @@ function roll(){
         if (ekaluku == 0){
             document.getElementById("alertArea").innerHTML = "You rolled 1 ! Zero points!";
         }
-        console.log(pisteet);
-        //points = points + ekaluku + 1;
     }
     upDate();
-    //laskePisteet();
 }
 
 function laskePisteet(){
+    //vuoro = [0,1,2,3,4]; //Pelaajien vuorot players.length  --> vuoron vaihtuminen
     let pituus = pisteet.length;
     for (let i = 0; i < pisteet.length; i++){
         points = points + pisteet[i];
     }
     points = points + pituus;
-    pointArray = [];
-    pointArray.push(points);
-    players[0].points = pointArray[0];
-    upDate();
+    players[vuoro].points = points;
+    if (vuoro < players.length){
+        vuoro++;
+    }
+    else if (vuoro > players.length){
+        vuoro== 0;
+    }
+    points == 0;
+    upDate(); 
 }
 
 function stopRoll(){
